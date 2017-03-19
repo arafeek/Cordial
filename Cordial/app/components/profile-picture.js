f711d289f14c855438e99d9f679bf79c1036efc2
arafeek <adam_rafeek@hotmail.com>
2017-03-19 18:20:54 -0400

    Make images persist

--- Cordial/app/components/profile-picture.js
+++ Cordial/app/components/profile-picture.js
@@ -8,53 +8,39 @@ var ImagePicker = require('react-native-image-picker');
 
 import {brightBlue, DEFAULT_PROFILE_IMAGE} from '../consts/styles';
 
-function uriFromID(id) { // TODO: This is a stub
-	return `img/${id}`;
-}
-
 export default class ProfilePicture extends Component {
 	constructor(props) {
 		super(props);
 
-		const {imgID} = props;
 		this.props = props;
+    const {uri} = this.props;
+    // unfortunately, using camera roll is pretty side-effecty
+    // so it's easier to just use state to manage this
 		this.state = {
-			uri: imgID ? uriFromID(imgID) : DEFAULT_PROFILE_IMAGE
+			uri: uri || DEFAULT_PROFILE_IMAGE,
 		};
 	}
 
   loadImagePicker() {
-    console.log('opened image picker');
-    ImagePicker.showImagePicker({}, (response) => {
-      console.log('Response, ', response);
-      if (response.uri) {
-        this.setState({
-          uri: response.uri,
-        });
-      }
-    });
+    let { editable, onChange } = this.props;
+    if (editable) {
+      ImagePicker.showImagePicker({}, (response) => {
+        if (response.uri) {
+          this.setState({
+            uri: response.uri,
+          });
+          onChange(response.uri);
+        }
+      });
+    }
   }
 
 	render() {
 		const {size = 50, borderWidth = 4} = this.props;
-		const {uri} = this.state;
-		return (
-			<View
-				style={[{
-					height: size,
-					width: size,
-					borderColor: brightBlue,
-					borderRadius: size,
-					borderWidth: borderWidth,
-					padding: 0,
-					margin: 0
-				},
-					this.props.style
-				]}
-			>
-        <TouchableOpacity
-          onPress={() => {this.loadImagePicker()}}
-          activeOpacity={0.7}>
+    const uri = this.props.uri || this.state.uri;
+    const { editable } = this.props;
+
+    var ImageElement = ( 
           <Image
             source={{uri}}
             style={{
@@ -63,10 +49,54 @@ export default class ProfilePicture extends Component {
               resizeMode: 'stretch',
               borderRadius: (size / 2) - 5
             }}
-          />
-        </TouchableOpacity>
-			</View>
-		);
+          />);
+
+    if (editable) {
+
+      return (
+        <View
+          style={[{
+            height: size,
+            width: size,
+            borderColor: brightBlue,
+            borderRadius: size,
+            borderWidth: borderWidth,
+            padding: 0,
+            margin: 0
+          },
+            this.props.style
+          ]}
+        >
+          <TouchableOpacity
+            onPress={() => {this.loadImagePicker()}}
+            activeOpacity={0.7}>
+
+            { ImageElement }
+
+          </TouchableOpacity>
+        </View>
+      );
+    }
+    else {
+      return (
+        <View
+          style={[{
+            height: size,
+            width: size,
+            borderColor: brightBlue,
+            borderRadius: size,
+            borderWidth: borderWidth,
+            padding: 0,
+            margin: 0
+          },
+            this.props.style
+          ]}
+        >
+          { ImageElement }
+        </View>
+      );
+    }
+
 	}
 }
 
