bf2a1b8c546334d07ac03179b5ef195cf745d093
Andrew Gemmel <a1gemmel@users.noreply.github.com>
2017-03-21 21:13:34 -0400

    Remove linear gradient from project (#86)

--- Cordial/android/app/src/main/java/com/cordial/MainApplication.java
+++ Cordial/android/app/src/main/java/com/cordial/MainApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -29,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RCTCameraPackage(),
-            new VectorIconsPackage(),
-            new LinearGradientPackage()
+            new VectorIconsPackage()
       );
     }
   };
