2c98414be52ae307d3f765b3b4e63070de35815e
Andrew Gemmel <a1gemmel@users.noreply.github.com>
2017-03-27 15:48:56 -0400

    Colored icons (#97)
    * Adds color data for brand icons
    
    * Manually scale some larger icons down

--- Cordial/app/components/touchable-icon.js
+++ Cordial/app/components/touchable-icon.js
@@ -2,19 +2,29 @@ import React, {Component} from 'react';
 import { TouchableOpacity, StyleSheet, View } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
+import Icons from '../consts/icons';
 import {white} from '../consts/styles';
 
 const styles = StyleSheet.create({
 	icon: {
 		backgroundColor: white,
 		borderWidth: 0,
-		borderRadius: 4
+		borderRadius: 4,
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 });
 
 export class Icon extends Component {
 	render() {
-		const {name, size, color} = this.props;
+		let {name, size, color} = this.props;
+
+		if (Icons[name]) {
+			if (!color) {
+				color = Icons[name].color;
+			}
+			size *= Icons[name].scale || 1; // scale icons to somewhat standard width
+		}
 		return (
 			<View style={[styles.icon, this.props.style]}>
 				<FontAwesomeIcon name={name} size={size} color={color} style={this.props.style}/>
