1a6a98797ff994d839e1aecda56180f52280c4de
arafeek <adam_rafeek@hotmail.com>
2017-03-28 15:41:56 -0400

    Added sanitization to auto-link-text

--- Cordial/app/components/auto-link-text.js
+++ Cordial/app/components/auto-link-text.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import Autolink from 'react-native-autolink';
-
+import { sanitizePhoneNumber } from '../utils/utils';
 
 export default class AutoLinkText extends Component {
 	render() {
 		const {style, numberOfLines, children = ''} = this.props;
+    const sanitized = sanitizePhoneNumber(children);
 		return (
 			<Autolink
 				phone email
 				linkStyle={ [{ color: '#2980b9'}, style] }
 				style={style}
-				text={children}
+				text={sanitized}
 				numberOfLines={numberOfLines}
 			/>
 		);
